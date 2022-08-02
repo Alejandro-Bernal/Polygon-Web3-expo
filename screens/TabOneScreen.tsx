767ae6a08bd0f51539e335d1a973822591a9087a
Angel Remigio <angelremigio@Angels-MacBook-Pro.local>
2022-08-02 10:29:04 -0700

    got rid of connect not a function

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,68 +1,96 @@
-import React from "react";
-import { StyleSheet, TouchableOpacity, Button } from "react-native";
-
-import EditScreenInfo from "../components/EditScreenInfo";
-import { Text, View } from "../components/Themed";
+import * as React from "react";
+import { Text, TouchableOpacity, StyleSheet, Platform, View, StatusBar } from "react-native";
+import { useWalletConnect } from "@walletconnect/react-native-dapp";
+import { WalletConnectProvider } from "@walletconnect/react-native-dapp/dist/providers";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { RootTabScreenProps } from "../types";
 
-import {
-  useWalletConnect,
-  WalletConnectContextValue,
-} from "@walletconnect/react-native-dapp";
-
-const shortenAddress = (address: string) => {
+const shortenAddress = (address : any) => {
   return `${address.slice(0, 6)}...${address.slice(
     address.length - 4,
     address.length
   )}`;
 };
 
-export default function TabOneScreen({
-  navigation,
-}: RootTabScreenProps<"TabOne">) {
-  const connector = useWalletConnect();
-  
-  if (!connector.connected) {
-    return <Button title="Connect" onPress={() => connector.connect()} />;
-  }
+function Button({ press, label } : any) {
   return (
-    <Button title="Kill Session" onPress={() => connector.killSession()} />
+    <TouchableOpacity onPress={press} style={styles.button}>
+      <Text style={styles.text}>{label}</Text>
+    </TouchableOpacity>
   );
 }
 
+function ConnectToWallet() {
+  const connector = useWalletConnect();
+
+  const connectWallet = React.useCallback(() => {
+    console.log("connecting")
+    return connector.connect();
+  }, [connector]);
+
+  const killSession = React.useCallback(() => {
+    return connector.killSession();
+  }, [connector]);
+
+  return (
+    
+    <>
+      {!connector.connected ? (
+        <Button press={connectWallet} label="Connect a wallet" />
+      ) : (
+        <>
+          <Text>{shortenAddress(connector.accounts[0])}</Text>
+          <Button press={killSession} label="Log out" />
+        </>
+      )}
+    </>
+  );
+}
+
+
+const SCHEME_FROM_APP_JSON = "walletconnect-example";
+
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  return(
+    <WalletConnectProvider
+      redirectUrl={
+        Platform.OS === "web"
+          ? window.location.origin
+          : `${SCHEME_FROM_APP_JSON}://`
+      }
+      storageOptions={{
+        asyncStorage: AsyncStorage,
+      }}
+    >
+      <View style={styles.container}>
+
+        <ConnectToWallet />
+
+        
+      </View>
+    </WalletConnectProvider>
+  )
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-  buttonStyle: {
-    backgroundColor: "#3399FF",
-    borderWidth: 0,
+  button: {
+    backgroundColor: "#5A45FF",
     color: "#FFFFFF",
-    borderColor: "#3399FF",
-    height: 40,
-    alignItems: "center",
-    borderRadius: 30,
-    marginLeft: 35,
-    marginRight: 35,
-    marginTop: 20,
-    marginBottom: 20,
+    borderRadius: 12,
+    paddingHorizontal: 16,
+    paddingVertical: 12,
   },
-  buttonTextStyle: {
+  text: {
     color: "#FFFFFF",
-    paddingVertical: 10,
-    paddingHorizontal: 15,
     fontSize: 16,
     fontWeight: "600",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
